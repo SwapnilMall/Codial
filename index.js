a85050f0aae0226e3589b31eaa5f6d447ed15e96
Swapnil Mall <swapnilmall03@gmail.com>
2020-05-28 21:28:39 +0530

    Created directory structure and set up express server

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8000;
+
+app.listen(port, (err) => {
+	if (err) {
+		console.log(`Error in running the server: ${err}`); //Interpolation
+		return;
+	}
+	console.log(`server running at port : ${port}`);
+});
